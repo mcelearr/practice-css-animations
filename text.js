014e5851a969bfd94687b2b9dba3ce302e707a82
mcelearr <rorymcelearney@gmail.com>
2016-06-26 20:40:05 +0100

    having some issues dynamically creating content

--- text.js
+++ text.js
@@ -1,15 +1,17 @@
 (function(){
-  var col1 = document.getElementsByClassName("col-1")[0];
-  var col2 = document.getElementsByClassName("col-1")[2];
-  var i = 0;
+  var para = document.createElement("p");
+  var text = document.createTextNode("This is how it all ends");
+  var pTextNode = para.appendChild(text);
+  var col1 = document.getElementById("column-left");
+  var col2 = document.getElementById("column-right");
 
-  // while (i < 28) {
-  //   setTimeout(function(){
-  //     var para = document.createElement("p");
-  //     var text = document.createTextNode("This is how it all ends");
-  //     col1.appendChild(para);
-  //     para.appendChild(text);
-  //     i++;
-  //   }, 500);
-  // }
+  for (var i = 0; i < 28; i ++) {
+    if (i % 2 === 0) {
+      col1.appendChild(pTextNode);
+    } else {
+      col2.appendChild(pTextNode);
+    }
+    i++;
+  }
+
 })()
