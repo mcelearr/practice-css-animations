884a414a1d698e39fd88bc52019cbf6575c19b4f
mcelearr <rorymcelearney@gmail.com>
2016-06-26 15:15:01 +0100

    sets up face to move around div

---
+++ animate.js
@@ -0,0 +1,15 @@
+(function(){
+  var face = document.getElementById("face");
+  var angle = 0;
+  var lastTime = null;
+
+  function animate(time) {
+    angle += (time - lastTime) * 0.0025;
+    lastTime = time;
+    face.style.top = (Math.sin(angle) * 100) + "px";
+    face.style.left = (Math.cos(angle) * 200) + "px";
+    requestAnimationFrame(animate);
+  }
+
+  requestAnimationFrame(animate);
+})()
